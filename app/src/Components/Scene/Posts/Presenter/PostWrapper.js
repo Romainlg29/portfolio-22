e3a6b8d842e147a2412550d749e162a4fd9dab26
Romain <legall.romain29470@gmail.com>
2022-03-19 21:53:47 +0100

    Random mesh generation into posts

--- app/src/Components/Scene/Posts/Presenter/PostWrapper.js
+++ app/src/Components/Scene/Posts/Presenter/PostWrapper.js
@@ -1,19 +1,11 @@
-import { MotionCanvas } from "framer-motion/three";
-import {
-  Scroll,
-  ScrollControls,
-  Preload,
-  Torus,
-  Sphere,
-  Tetrahedron,
-} from "@react-three/drei";
+import { Scroll, ScrollControls, Preload } from "@react-three/drei";
 import Lights from "../../Lights";
-import { RoughMaterialRoyalBlue } from "../../Materials";
-import RoundedCube from "../../Cubes/RoundedCube";
+import { Canvas } from "@react-three/fiber";
+import RandomMesh from "../../../Utils/RandomMesh";
 
 const PostWrapper = ({ pages = 1, children }) => {
   return (
-    <MotionCanvas
+    <Canvas
       colorManagement
       dpr={[1, 2]}
       camera={{ fov: 70 }}
@@ -26,41 +18,20 @@ const PostWrapper = ({ pages = 1, children }) => {
       <ScrollControls pages={pages} damping={4} distance={2}>
         <Scroll html>{children}</Scroll>
         <Scroll>
-          <Torus
-            position={[4.5, -5, 1]}
-            rotation={[Math.PI / 2, Math.PI / 6, 0]}
-            scale={0.2}
-            args={[2, 1, 48, 64]}
-            material={RoughMaterialRoyalBlue}
-          />
-          <RoundedCube
-            position={[-6, -15, -1]}
-            rotation={[-Math.PI / 2, Math.PI / 4, 0]}
-            scale={0.7}
-            material={RoughMaterialRoyalBlue}
-          />
-          <Sphere
-            position={[7, -25, -2]}
-            scale={0.5}
-            material={RoughMaterialRoyalBlue}
-          />
-          <Tetrahedron
-            position={[-4, -38.5, 1]}
-            rotation={[Math.PI / 4, 0, Math.PI / 2]}
-            scale={0.25}
-            material={RoughMaterialRoyalBlue}
-          />
-          <RoundedCube
-            position={[6, -48, -1]}
-            rotation={[Math.PI / 4, Math.PI / 2, 0]}
-            scale={0.7}
-            material={RoughMaterialRoyalBlue}
-          />
+          {Array(pages === 0 ? 1 : Math.ceil(pages * 1.5))
+            .fill(0)
+            .map((_, k) => {
+              return (
+                <RandomMesh
+                  key={`RandomMesh_${k}`}
+                  position={[k % 2 === 0 ? 5.5 : -6, (k + 1) * -5, -1]}
+                />
+              );
+            })}
         </Scroll>
       </ScrollControls>
-
       <Lights />
-    </MotionCanvas>
+    </Canvas>
   );
 };
 export default PostWrapper;
