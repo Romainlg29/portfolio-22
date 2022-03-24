f61fcb041d90364b076a7c5a9d1f9b211ee23fb5
Romain <legall.romain29470@gmail.com>
2022-03-24 09:32:32 +0100

    concurrent mode

--- app/src/index.js
+++ app/src/index.js
@@ -8,6 +8,17 @@ import ThreeRouter from "./Components/Routes/ThreeRouter";
 
 import { history } from "./Components/Routes/History";
 
+/*ReactDOMClient.hydrateRoot(
+  document.getElementById("root"),
+  <React.StrictMode>
+    <HelmetProvider>
+      <ThreeRouter history={history}>
+        <App />
+      </ThreeRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+);*/
+
 ReactDOMClient.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
